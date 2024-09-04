3f1da1795c8c4122c9357584d02c62cd9c21d5f8
Aman34772 <amankumar34772@gmail.com>
2024-09-04 12:42:26 +0530

    Your commit message here

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const { dbConnect } = require("./connection");
 // Route setup
+const cors = require("cors");
 const UserRouter = require("./routes/UserRouter");
 // const data = require("./models/MOCK_DATA.json")
 const app = express();
@@ -11,6 +12,16 @@ dbConnect(
   "mongodb+srv://amankumar:amankumar@cluster0.auljv.mongodb.net/test"
 ).then(() => console.log("mongodb connected"));
 
+app.use(cors());
+
+// OR - Enable CORS with specific settings
+app.use(
+  cors({
+    origin: "*", // Replace with your frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+  })
+);
 // Middleware to parse JSON requests
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
